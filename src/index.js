79e3e3299d6a257a812c55f04443ff3d6205cb58
VilkhovyiDmytro <dmitrovilhovij@gmail.com>
2023-06-13 14:31:54 +0300

    commit beffore events

--- src/index.js
+++ src/index.js
@@ -3,42 +3,59 @@ import ReactDOM from "react-dom/client";
 
 import "./banana.css";
 
-const author = "Dmytro Vilkhovyi";
-const title = "some book";
-const img = "./img/shopping(0).webp";
-
-
+const books = [
+  {
+    author:"Bandera",
+    title:"Nash Batko",
+    img:"./img/shopping(0).webp",
+    id:1,
+  },
+  {
+    author:"Vunnuchenko",
+    title:"beter would wrote",
+    id:2,
+    img:"./img/shopping(1).webp"
+  },
+  {
+    author:"Chornovil",
+    title:"son is dodik",
+    img:"./img/shopping(2).webp",
+    id:3,
+  }
+]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book title={title} img={img} author={author}/>
-      <Book title={title} img={img} author={author}/>
-      <Book title={title} img={img} author={author}/>
+      {books.map((book)=>{
+        return <Book {...book} key={book.id}/>
+      })}
     </section>
   );
 };
+
+
 
 const Book = (props) => {
   console.log(props)
   return (
     <article className="book">
       <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
       <h2>{props.author}</h2>
+      <h2>{props.title}</h2>
     </article>
   );
 };
 
-const Image = () => {
-  return <img src={img} alt={title} />;
-};
-const Title = () => {
-  return <h2>{title}</h2>;
-};
-const Author = () => {
-  return <h2>{author}</h2>;
-};
+// const Image = () => {
+//   return <img src={img} alt={title} />;
+// };
+// const Title = () => {
+//   return <h2>{title}</h2>;
+// };
+// const Author = () => {
+//   return <h2>{author}</h2>;
+// };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
